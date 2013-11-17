e9a657115ed7bcde452bbb246e14f1f8077fcef9
Matthew Maycock <ummaycoc@gmail.com>
2013-11-17 11:24:57 -0500

    Uses Inspector module better naming

--- ray/streams.ml
+++ ray/streams.ml
@@ -1,28 +1,31 @@
 let debug_print tokens =
-  Inspector.print_tokens "Input:      " tokens;
-  let tokens = WhiteSpace.drop_eof tokens in
-  Inspector.print_tokens "No EOF      " tokens;
-  let tokens = WhiteSpace.indenting_space tokens in
-  Inspector.print_tokens "Indented:   " tokens;
-  let tokens = WhiteSpace.despace_brace tokens in
-  Inspector.print_tokens "In-Brace:   " tokens;
-  let tokens = WhiteSpace.trim_lines tokens in
-  Inspector.print_tokens "Trimmed:    " tokens;
-  let tokens = WhiteSpace.squeeze_lines tokens in
-  Inspector.print_tokens "Squeezed:   " tokens;
-  let lines = WhiteSpace.tokens_to_lines tokens in
-  Inspector.print_lines  "Lines:      " lines;
-  let lines = WhiteSpace.merge_lines lines in
-  Inspector.print_lines  "Merged:     " lines;
-  let lines = WhiteSpace.block_merge lines in
-  Inspector.print_lines  "Blocks:     " lines;
-  let tokens = WhiteSpace.space_to_brace lines in
-  Inspector.print_tokens "Converted:  " tokens;
-  let tokens = WhiteSpace.append_eof tokens in
-  Inspector.print_tokens "With EOF:   " tokens
+  let ptoken = Inspector.pprint_token_list in
+  let plines = Inspector.pprint_token_lines in begin
+    ptoken "Input:      " tokens;
+    let tokens = WhiteSpace.drop_eof tokens in
+    ptoken "No EOF      " tokens;
+    let tokens = WhiteSpace.indenting_space tokens in
+    ptoken "Indented:   " tokens;
+    let tokens = WhiteSpace.despace_brace tokens in
+    ptoken "In-Brace:   " tokens;
+    let tokens = WhiteSpace.trim_lines tokens in
+    ptoken "Trimmed:    " tokens;
+    let tokens = WhiteSpace.squeeze_lines tokens in
+    ptoken "Squeezed:   " tokens;
+    let lines = WhiteSpace.tokens_to_lines tokens in
+    plines "Lines:      " lines;
+    let lines = WhiteSpace.merge_lines lines in
+    plines "Merged:     " lines;
+    let lines = WhiteSpace.block_merge lines in
+    plines "Blocks:     " lines;
+    let tokens = WhiteSpace.space_to_brace lines in
+    ptoken "Converted:  " tokens;
+    let tokens = WhiteSpace.append_eof tokens in
+    ptoken "With EOF:   " tokens
+  end
 
 let _ =
   let tokens = Inspector.from_channel stdin in
   match Array.length Sys.argv with
-    | 1 -> Inspector.print_tokens "" (WhiteSpace.convert tokens)
+    | 1 -> Inspector.pprint_token_list "" (WhiteSpace.convert tokens)
     | _ -> debug_print tokens
