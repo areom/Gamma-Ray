be43e9211c57fa0ea448685fd1c0392711b0c827
Matthew Maycock <ummaycoc@gmail.com>
2013-11-17 19:33:57 -0500

    Added unscan program

---
+++ ray/unscan.ml
@@ -0,0 +1,48 @@
+open Parser
+
+let descan = Inspector.descan
+
+let rec indenter depth indent =
+  for i = 1 to depth do print_string indent done
+
+(* Unscan a sequence of tokens. Requires sanitized stream *)
+let rec clean_unscan depth indent = function
+  (* ARRAY / LBRACKET RBRACKET ambiguity... *)
+  | LBRACKET::RBRACKET::rest ->
+    print_string ((descan LBRACKET) ^ " " ^ (descan RBRACKET));
+    clean_unscan depth indent rest
+  | LBRACE::rest ->
+    print_string (descan LBRACE);
+    print_newline ();
+    indenter (depth+1) indent;
+    clean_unscan (depth+1) indent rest
+  | SEMI::RBRACE::rest ->
+    print_string (descan SEMI);
+    clean_unscan depth indent (RBRACE::rest)
+  | RBRACE::RBRACE::rest ->
+    print_newline ();
+    indenter (max (depth-1) 0) indent;
+    print_string (descan RBRACE);
+    clean_unscan (max (depth-1) 0) indent rest
+  | RBRACE::rest ->
+    print_newline ();
+    indenter (depth-1) indent;
+    print_string (descan RBRACE);
+    print_newline ();
+    indenter (depth-1) indent;
+    clean_unscan (max (depth-1) 0) indent rest
+  | SEMI::rest ->
+    print_string (descan SEMI);
+    print_newline ();
+    indenter depth indent;
+    clean_unscan depth indent rest
+  | token::rest ->
+    print_string (descan token);
+    print_string " ";
+    clean_unscan depth indent rest
+  | [] ->
+    print_newline ()
+
+let _ =
+  let tokens = Inspector.from_channel stdin in
+  clean_unscan 0 "  " (WhiteSpace.convert tokens)
