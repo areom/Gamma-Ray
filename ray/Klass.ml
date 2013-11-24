3dd5dae7b89052ffff594ba7645d05c8ce0bb8ad
Matthew Maycock <ummaycoc@gmail.com>
2013-11-24 00:57:39 -0500

    a bit more cleaning (spaces, rearranging, etc)

--- ray/Klass.ml
+++ ray/Klass.ml
@@ -129,17 +129,64 @@ let build_class_var_map klasses =
       | Right(collisions) -> (klass_map, (aklass, collisions)::collision_list) in
   build_map_track_errors map_builder klasses
 
+(* Given a class -> var map table as above, do a lookup -- returns option *)
+let class_var_lookup map klassName varName =
+  match (map_lookup klassName map) with
+    | Some(varTable) -> map_lookup varName varTable
+    | None -> None
+
+(* Builds a map of all the methods within a class
+ * Key = function name
+ * value = list of accessmethod,fdef pairs
+
+ * match_args checks if there is an fdef already in the map,
+ * irrespective of its accessmethod, which matches the given
+ * method of the class. If no collisions, add the (function def,
+ * accessmethod) pair to the list of fdef with the same function
+ * name or make a new entry. If the fdef is ambiguous, add the
+ * fdef to the collision list
+ *)
+
+let build_method_map aklass =
+  let add_method access (map,collisions) fdef =
+    let found = function
+      | Some _ -> true
+      | None   -> false in
+    let get_match_args = match_args fdef.formals (StringMap.find fdef.name map) in
+    if ((StringMap.mem fdef.name map) && (found get_match_args)) then
+      then (map, (access,fdef)::collisions)
+      else ((add_map_list fdef.name (access,fdef) map), collisions) in
+   let map_builder map_pair (access, fdeflist) = List.fold_left (add_method access) map_pair fdeflist in
+   build_map_track_errors map_builder (klass_to_methods aklass)
+
+(* Builds a map from classname to map of list of function definition, accessmode pair
+ * Key = classname, value = method_map ( from build_method_map function) ,
+ * destination map = klass_map.
+ * If the method_map construction found collisions, i.e there were function
+ * definitions with same name and exactly same argument types, then this method
+ * will return the collision list of pairs of (classname , list colliding function def
+ * and access mode pair)
+ *)
+let build_class_method_map klasses =
+  let map_builder (klass_map, collision_list) aklass =
+    match(build_method_map aklass) with
+      | Left(method_map) -> (StringMap.add (aklass.klass) method_map klass_map, collision_list)
+      | Right(collisions) -> (klass_map, (aklass, collisions)::collision_list) in
+  build_map_track_errors map_builder klasses
+
+
+
 (* Checks if two variables are of same type *)
 let same_type typ1 typ2 = (typ1 = typ2)
 
 (*Checks if the formal arguments are ambiguous.
  *Pass the appropriate check_ambiguous function.
- *)	
+ *)
 let rec match_formals list1 list2 =
   try List.for_all2 same_type list1 list2 with
     | InvalidArgument -> false
 
-let rec match_args arglist fdeflist =  match fdeflist with 
+let rec match_args arglist fdeflist =  match fdeflist with
   | [] ->  None
   | (access,fn)::tl ->
     if (match_formals arglist fn.formals)
@@ -154,7 +201,7 @@ i.e typ1 is same as typ2 or typ1 is same as typ2's child and so on
   else we return (false, -1)
   If typ1 and typ2 are both same level will be 0
   else it will be the number of levels by which they are apart
-**)	
+**)
 
 let rec isSubtype typ1 typ2 level sub_to_super_map  =
   if typ1 = typ2  then level
@@ -162,8 +209,8 @@ let rec isSubtype typ1 typ2 level sub_to_super_map  =
     else if(StringMap.mem typ1 sub_to_super_map) then
       isSubtype (StringMap.find typ1 sub_to_super_map) typ2 (level + 1) sub_to_super_map
     else -1
-		
-		
+
+
 
 (* Given two argument list, score them on how closely they match with each other
    The first list is the actual arguments, the second list is the formal arguments
@@ -172,7 +219,7 @@ let rec getscore list1 list2 map = match list1, list2 with
   | [], [] ->  [Some(0)]
   | (hd,_)::tl, (x,_)::y -> Some(isSubtype hd x 0 map)::(getscore tl y map)
   | _, _ -> [Some(-1)]
- 
+
 
 (*Given a list of samelength fdefs check if they are compatible
  *Add the fdefs with the compatibility score and build the list.
@@ -204,63 +251,16 @@ let get_method fdef fdeflist map filtermap =
       | [] -> None
       | hd::tl -> None (* (score_of hd current),(fdef_of hd)  *)
 
-(* Builds a map of all the methods within a class
- * Key = function name
- * value = list of accessmethod,fdef pairs
-
- * match_args checks if there is an fdef already in the map,
- * irrespective of its accessmethod, which matches the given
- * method of the class. If no collisions, add the (function def,
- * accessmethod) pair to the list of fdef with the same function
- * name or make a new entry. If the fdef is ambiguous, add the 
- * fdef to the collision list
- *)
- 
-let build_method_map aklass =
-  let add_method access (map,collisions) fdef =
-    let found = function
-      | Some _ -> true
-      | None   -> false in
-    let get_match_args = match_args fdef.formals (StringMap.find fdef.name map) in
-    if ((StringMap.mem fdef.name map) && (found get_match_args)) then
-      then (map, (access,fdef)::collisions)
-      else ((add_map_list fdef.name (access,fdef) map), collisions) in
-   let map_builder map_pair (access, fdeflist) = List.fold_left (add_method access) map_pair fdeflist in
-   build_map_track_errors map_builder (klass_to_methods aklass)
-
-(* Builds a map from classname to map of list of function definition, accessmode pair
- * Key = classname, value = method_map ( from build_method_map function) , 
- * destination map = klass_map.
- * If the method_map construction found collisions, i.e there were function
- * definitions with same name and exactly same argument types, then this method
- * will return the collision list of pairs of (classname , list colliding function def 
- * and access mode pair)
- *)
-let build_class_method_map klasses = 
-  let map_builder (klass_map, collision_list) aklass =
-    match(build_method_map aklass) with
-      | Left(method_map) -> (StringMap.add (aklass.klass) method_map klass_map, collision_list)
-      | Right(collisions) -> (klass_map, (aklass, collisions)::collision_list) in
-  build_map_track_errors map_builder klasses
-
-
-(* Given a class -> var map table as above, do a lookup -- returns option *)
-let class_var_lookup map klassName varName =
-  match (map_lookup klassName map) with
-    | Some(varTable) -> map_lookup varName varTable
-    | None -> None
 
 (*Looks up list of function definitions matching the given function name*)
 (*
 let method_lookup funcName argList methodmap =
-	if (StringMap.mem funcName methodmap)  then
-		match_args similar_type argList (StringMap.find funcName methodmap)
-	else
-		(false, None)
+  if (StringMap.mem funcName methodmap)
+    then match_args similar_type argList (StringMap.find funcName methodmap)
+    else (false, None)
 
 (*Given a class -> method map table , do a fn lookup -- returns option *)
 let class_method_lookup map className funcName argtype_list =
-  match (map_lookup className map) with 
-	|Some(method_map) -> method_lookup funcName argtype_list method_map
-	|None  -> None
-*)
+  match (map_lookup className map) with
+    | Some(method_map) -> method_lookup funcName argtype_list method_map
+    | None  -> None
