b0a606c6f1353fcf83078f7433e93493e4bfd508
Matthew Maycock <ummaycoc@gmail.com>
2013-11-29 00:45:22 -0500

    Moved some generic stuff to Util; organized all the tables into one record that gets passed around

--- ray/Klass.ml
+++ ray/Klass.ml
@@ -1,41 +1,34 @@
 open Ast
+open Util
 module StringMap = Map.Make (String)
 
 (* Types *)
 type access_mode = Public | Protected | Private
-type method_section = Public | Protected | Private | Refines | Mains
-type ('a, 'b) either = Left of 'a | Right of 'b
+type method_section = Publics | Protects | Privates | Refines | Mains
 
-(* Generic helper functions *)
+(* Just for convenient reading *)
+type 'a lookup_table = 'a StringMap.t StringMap.t
+type 'a lookup_map = 'a StringMap.t
 
-(* Reduce a list of options to the values in the Some constructors *)
-let filter_option list =
-  let rec do_filter rlist = function
-    | [] -> List.rev rlist
-    | None::tl -> do_filter rlist tl
-    | (Some(v))::tl -> do_filter (v::rlist) tl in
-  do_filter [] list
+type class_data = {
+  classes : class_def lookup_map;
+  parents : string lookup_map;
+  children : (string list) lookup_map;
+  variables : (access_mode * string) lookup_table;
+  methods : ((method_section * func_def) list) lookup_table;
+  ancestors : (string list) lookup_map;
+  distance : int lookup_table;
+}
 
-(* Lexically compare two lists of comparable items *)
-let rec lexical_compare list1 list2 = match list1, list2 with
-  | [], [] -> 0
-  | [], _  -> -1
-  | _, []  -> 1
-  | (x::xs), (y::ys) -> if x < y then -1 else if x > y then 1 else lexical_compare xs ys
-
-(* Loop through a list and find all the items that are minimum with respect to the total
- * ordering cmp. Note can return any size list.
- *)
-let find_all_min cmp alist =
-  let rec min_find found items =
-    match found, items with
-      | _, [] -> List.rev found (* Return in the same order at least *)
-      | [], i::is -> min_find [i] is
-      | (f::fs), (i::is) -> let result = cmp i f in
-        if result = 0 then min_find (i::found) is
-        else if result < 0 then min_find [i] is
-        else min_find found is in
-  min_find [] alist
+let empty_data : class_data = {
+  classes = StringMap.empty;
+  parents = StringMap.empty;
+  children = StringMap.empty;
+  variables = StringMap.empty;
+  methods = StringMap.empty;
+  ancestors = StringMap.empty;
+  distance = StringMap.empty;
+}
 
 (* Builder should accept a StringMap, errors list pair and either return an updated
  * map or an updated error list in the new pair (but hopefully not both). The list
@@ -86,35 +79,35 @@ let klass_to_methods aklass =
     | InitMem(i) -> Some(i) in
   let funcs members = filter_option (List.map to_function members) in
   let s = aklass.sections in
-  [(Public, funcs s.publics); (Protected, funcs s.protects); (Private, funcs s.privates); (Refines, s.refines); (Mains, s.mains)]
+  [(Publics, funcs s.publics); (Protects, funcs s.protects); (Privates, funcs s.privates); (Refines, s.refines); (Mains, s.mains)]
 
 (* make a map children map
  *   parent (name -- string) -> children (names -- string) list
  *)
-let build_children_map klasses =
+let build_children_map data klasses =
   let map_builder map aklass = add_map_list (klass_to_parent aklass) (aklass.klass) map in
-  List.fold_left map_builder StringMap.empty klasses
+  let children_map = List.fold_left map_builder StringMap.empty klasses in
+  { data with children = children_map }
 
-(* make a map of subclasses to superclasses
- *   class (string) -> parent (string)
- *)
-let build_parent_map klasses =
+(* lookup_map that yields parents *)
+let build_parent_map data klasses =
   let map_builder map aklass =
     let parent = klass_to_parent aklass in
     let child  = aklass.klass in
     StringMap.add parent child map in
-  List.fold_left map_builder StringMap.empty klasses
+  let parent_map = List.fold_left map_builder StringMap.empty klasses in
+  { data with parents = parent_map }
 
 (* Validate that the klass map represents a tree -- everything should go
  * back to object eventually. Returns string option reporting an issue when
  * there is an issue to report.
  *)
-let is_tree_hierarchy parent_map =
+let is_tree_hierarchy data =
   let rec from_object klass checked =
     match map_lookup klass checked with
       | Some(true) -> Left(checked)
       | Some(false) -> Right("Cycle detected.")
-      | _ -> match map_lookup klass parent_map with
+      | _ -> match map_lookup klass data.parents with
         | None -> Right("Cannot find parent after building parent map: " ^ klass)
         | Some(parent) -> match from_object parent (StringMap.add klass false checked) with
           | Left(updated) -> Left(StringMap.add klass true updated)
@@ -123,14 +116,15 @@ let is_tree_hierarchy parent_map =
     | Left(checked) -> from_object klass checked
     | issue -> issue in
   let checked = StringMap.add "Object" true StringMap.empty in
-  match StringMap.fold folder parent_map (Left(checked)) with
+  match StringMap.fold folder data.parents (Left(checked)) with
     | Right(issue) -> Some(issue)
     | _ -> None
 
 (* Build class name -> class def map *)
-let build_class_map klasses =
+let build_class_map data klasses =
   let map_builder map aklass = StringMap.add (aklass.klass) aklass map in
-  List.fold_left map_builder StringMap.empty klasses
+  let class_map = List.fold_left map_builder StringMap.empty klasses in
+  { data with classes = class_map }
 
 (* For a given class, build a map of variable names to variable information.
  * If all instance variables are uniquely named, returns Left (map) where map
@@ -149,12 +143,14 @@ let build_var_map aklass =
  * instance variables in all the classes are okay. Otherwise returns Right
  * (collisions) where collisions is a list of pairs (class, colliding vars)
  *)
-let build_class_var_map klasses =
+let build_class_var_map data klasses =
   let map_builder (klass_map, collision_list) aklass =
     match build_var_map aklass with
       | Left(var_map) -> (StringMap.add (aklass.klass) var_map klass_map, collision_list)
       | Right(collisions) -> (klass_map, (aklass, collisions)::collision_list) in
-  build_map_track_errors map_builder klasses
+  match build_map_track_errors map_builder klasses with
+    | Left(variable_map) -> Left({ data with variables = variable_map })
+    | Right(collisions) -> Right(collisions) (* Same value different types parametrically *)
 
 (* Return whether two function definitions have conflicting signatures *)
 let conflicting_signatures func1 func2 =
@@ -185,36 +181,38 @@ let build_method_map aklass =
  * method signatures are okay. Otherwise returns Right (collisions) when the
  * method signatures collide.  Collisions are (class, colliding methods) pairs.
  *)
-let build_class_method_map klasses =
+let build_class_method_map data klasses =
   let map_builder (klass_map, collision_list) aklass =
     match build_method_map aklass with
       | Left(method_map) -> (StringMap.add (aklass.klass) method_map klass_map, collision_list)
       | Right(collisions) -> (klass_map, (aklass, collisions)::collision_list) in
-  build_map_track_errors map_builder klasses
+  match build_map_track_errors map_builder klasses with
+    | Left(method_map) -> Left({ data with methods = method_map })
+    | Right(collisions) -> Right(collisions) (* Same value different types parametrically *)
 
 (* Given a class -> variable map table lookup the (access mode, type) for a variable in a class.
  * Can return None if none found otherwise Some((access, type))
  *)
-let class_var_lookup map klass_name var_name =
-  match map_lookup klass_name map with
+let class_var_lookup data klass_name var_name =
+  match map_lookup klass_name data.variables with
     | Some(var_map) -> map_lookup var_name var_map
     | _ -> None
 
 (* Given a class -> method map table lookup the list of methods for a method name in a class.
  * Returns a list of methods with that name in the class (possibly empty)
  *)
-let class_method_lookup map klass_name func_name =
-  match map_lookup klass_name map with
+let class_method_lookup data klass_name func_name =
+  match map_lookup klass_name data.methods with
     | Some(method_map) -> map_lookup_list func_name method_map
     | _ -> []
 
 (* Given a class -> method map table lookup the access mode in a klass of the given func_def
  * Throws Invalid_argument if there is no such func in the klass
  *)
-let class_method_section_lookup klass_method_map klass_name func_def =
+let class_method_section_lookup data klass_name func_def =
   let err_msg = "Section lookup on nonexistant method: " ^ (signature_string func_def) ^ " of " ^ klass_name ^ ". Should not have happened -- compiler error." in
   let same_func (_, a_func) = conflicting_signatures a_func func_def in
-  match class_method_lookup klass_method_map klass_name func_def.name with
+  match class_method_lookup data klass_name func_def.name with
     | [] -> raise(Invalid_argument(err_msg ^ " [Class method lookup fail]"))
     | methods -> match List.filter same_func methods with
       | [] -> raise(Invalid_argument(err_msg ^ " [Signature search fail]"))
@@ -226,17 +224,18 @@ let class_method_section_lookup klass_method_map klass_name func_def =
  * element is the 0th ancestor, the class itself. The last ancestor is Object -- in between
  * the ancestors go from child to parent
  *)
-let build_ancestor_map parent_map =
+let build_ancestor_map data =
   let rec ancestor_builder klass map =
     if StringMap.mem klass map then map
     else
-      let parent = StringMap.find klass parent_map in
+      let parent = StringMap.find klass data.parents in
       let map = ancestor_builder parent map in
       let ancestors = StringMap.find parent map in
       StringMap.add klass (klass::ancestors) map in
   let folder klass _ map = ancestor_builder klass map in
   let map = StringMap.add "Object" ["Object"] StringMap.empty in
-  StringMap.fold folder parent_map map
+  let ancestor_map = StringMap.fold folder data.parents map in
+  { data with ancestors = ancestor_map }
 
 (* Given a klass and its list of ancestors, build a distance map to those
  * ancestors -- i.e. the number of hops back up the tree one must take to
@@ -249,7 +248,9 @@ let build_distance klass ancestors =
 (* Given an ancestor map (see build_ancestor_map), build a distance map
  * out of the keys and values
  *)
-let build_distance_map ancestor_map = StringMap.mapi build_distance ancestor_map
+let build_distance_map data = 
+  let distance_map = StringMap.mapi build_distance data.ancestors in
+  { data with distance = distance_map }
 
 (* Given a distance map and two classes, return the distance between the two classes.
  * If one is a proper subtype of the other then either a Some(n) where n is non-zero
@@ -259,12 +260,12 @@ let build_distance_map ancestor_map = StringMap.mapi build_distance ancestor_map
  * Note that a non-negative is returned if klass1 derives transitively from klass2
  * and a non-positive is returned if klass2 derives transitively from klass1
  *)
-let get_distance distance_map klass1 klass2 =
+let get_distance data klass1 klass2 =
   (* We lt these pop exceptions because that means bad programming on the compiler
    * writers part, not on the GAMMA programmer's part (when klass1, klass2 aren't found)
    *)
-  let klass1_map = StringMap.find klass1 distance_map in
-  let klass2_map = StringMap.find klass2 distance_map in
+  let klass1_map = StringMap.find klass1 data.distance in
+  let klass2_map = StringMap.find klass2 data.distance in
   match map_lookup klass2 klass1_map, map_lookup klass1 klass2_map with
     | None, None -> None
     | None, Some(n) -> Some(-n)
@@ -273,38 +274,38 @@ let get_distance distance_map klass1 klass2 =
 (* Given a distance map, determine if one type is a subtype of the other. Note that
  * this holds when the distance is zero and so when the two types are the same.
  *)
-let is_subtype distance_map subtype supertype =
-  match get_distance distance_map subtype supertype with
+let is_subtype data subtype supertype =
+  match get_distance data subtype supertype with
     | Some(n) when n >= 0 -> true
     | _ -> false
 
 (* Given a distance map, determine if one type is a strict subtype of the other. Note
  * that this means tha the two types are not the same.
  *)
-let is_proper_subtype distance_map subtype supertype =
-  match get_distance distance_map subtype supertype with
+let is_proper_subtype data subtype supertype =
+  match get_distance data subtype supertype with
     | Some(n) when n > 0 -> true
     | _ -> false
 
 (* Return whether a formals list is compatible with an actuals list
  * This includes checking that the number of arguments are the same, too.
  *)
-let compatible_formals distance_map actuals formals =
-  let compatible formal actual = is_subtype distance_map actual formal in
+let compatible_formals data actuals formals =
+  let compatible formal actual = is_subtype data actual formal in
   try List.for_all2 compatible formals actuals with
     | Invalid_argument(_) -> false
 
 (* A predicate for whether a func_def is compatible with an actuals list *)
-let compatible_function distance_map actuals func_def =
-  compatible_formals distance_map actuals (List.map fst func_def.formals)
+let compatible_function data actuals func_def =
+  compatible_formals data actuals (List.map fst func_def.formals)
 
 (* Given a list of methods and a list of actuals, find the best matches
  * Note that if there are ties for mest match, this returns multiple matches.
  * Whether this is an error depends on the caller.
  *)
-let best_matching_signature distance_map actuals funcs =
-  let funcs = List.filter (compatible_function distance_map actuals) funcs in
-  let distance_of actual formal = match get_distance distance_map actual formal with
+let best_matching_signature data actuals funcs =
+  let funcs = List.filter (compatible_function data actuals) funcs in
+  let distance_of actual formal = match get_distance data actual formal with
     | Some(n) when n >= 0 -> n
     | _ -> raise(Invalid_argument("Compatible methods somehow incompatible: " ^ actual ^ " vs. " ^ formal ^ ". Compiler error.")) in
   let to_distance func = List.map2 distance_of actuals (List.map fst func.formals) in
@@ -317,10 +318,10 @@ let best_matching_signature distance_map actuals funcs =
  * compiler error -- we should have already removed duplicate methods and so there
  * must be a unique lexicographic minimum.
  *)
-let best_method klass_method_map distance_map klass_name method_name actuals =
-  let methods = class_method_lookup klass_method_map klass_name method_name in
+let best_method data klass_name method_name actuals =
+  let methods = class_method_lookup data klass_name method_name in
   let no_sections = List.map snd methods in
-  match best_matching_signature distance_map actuals no_sections with
+  match best_matching_signature data actuals no_sections with
     | [] -> None
-    | [func] -> Some((class_method_section_lookup klass_method_map klass_name func, func))
+    | [func] -> Some((class_method_section_lookup data klass_name func, func))
     | _ -> raise(Invalid_argument("Multiple methods of the same signature in " ^ klass_name ^ "; Compiler error."))
