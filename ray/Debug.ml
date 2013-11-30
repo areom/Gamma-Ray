3b19a6286004008514e54adfe6ad14d91777e4e7
Matthew Maycock <ummaycoc@gmail.com>
2013-11-30 12:44:59 -0500

    Added something to get the longest method body (including recursive structure of statements)

--- ray/Debug.ml
+++ ray/Debug.ml
@@ -1,3 +1,4 @@
+open Ast
 
 let get_example_path dir example = String.concat Filename.dir_sep ["test"; "tests"; "Brace"; dir; example]
 
@@ -10,3 +11,11 @@ let get_example_scan dir example =
 let get_example_parse dir example =
   let tokens = get_example_scan dir example in
   Parser.cdecls (WhiteSpace.lextoks tokens) (Lexing.from_string "")
+
+let get_example_longest_block dir example =
+  let klasses = get_example_parse dir example in
+  let methods aklass = List.flatten (List.map snd (Klass.klass_to_methods aklass)) in
+  let all_methods = List.flatten (List.map methods klasses) in
+  let with_counts = List.map (function func -> (Util.get_statement_count func.body, func)) all_methods in
+  let maximum = List.fold_left max 0 (List.map fst with_counts) in
+  List.map snd (List.filter (function (c, _) -> c == maximum) with_counts)
