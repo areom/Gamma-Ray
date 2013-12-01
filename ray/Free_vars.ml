73f1a03c6fcd65941361613c84940f7c9b772edb
Weiyuan Li <liwy34@gmail.com>
2013-11-30 20:45:14 -0500

    Now Free_vars.ml handles body of Anonymous

--- ray/Free_vars.ml
+++ ray/Free_vars.ml
@@ -2,15 +2,15 @@ open Parser
 open Ast
 
 let _id  = function
-	| Null -> []
-	| x -> [x]
+	| None -> []
+	| Some(x) -> [x]
 
 let rec get_vars_expr = function
 	| Id(id) -> [id]
 	| This -> []
 	|	Null -> []
 	| NewObj(the_type, args) -> List.concat (List.map get_vars_expr args) 
-	| Anonymous(the_type, args, body) -> (List.concat (List.map get_vars_expr args)) (*@ (List.concat (List.map get_vars_func_def body))*)
+	| Anonymous(the_type, args, body) -> (List.concat (List.map get_vars_expr args)) @ (List.concat (List.map get_vars_func_def body))
 	| Literal(l) -> []
 	| Invoc(receiver, meth, args) -> (get_vars_expr receiver) @ (List.concat (List.map get_vars_expr args))
 	| Field(receiver, field) -> (get_vars_expr receiver) @ [field]
@@ -25,16 +25,19 @@ and apply_opt f = function
 	| Some(v) -> f v
 and get_vars_var_def (the_type, the_var) = [the_var]
 and get_vars_stmt = function
-	|	Decl(the_def, the_expr) -> apply_opt get_vars_expr the_expr
+	|	Decl(the_def, the_expr) -> (get_vars_var_def the_def) @ (apply_opt get_vars_expr the_expr)
 	| If(clauses) -> List.concat (List.map apply_clause clauses)
 	| While(pred, body) -> (get_vars_expr pred) @ (List.concat (List.map get_vars_stmt body)) 
 	| Expr(the_expr) -> get_vars_expr the_expr
 	| Return(the_expr) -> apply_opt get_vars_expr the_expr
 	| Super(args) -> List.concat (List.map get_vars_expr args)
 and apply_clause (opt_expr, body) = (apply_opt get_vars_expr opt_expr) @ (List.concat (List.map get_vars_stmt body))
-(*and get_vars_func_def func = List.concat [(apply_opt _id func.returns);(List.concat (List.map get_vars_var_def func.formals));(List.concat(List.map get_vars_stmt func.body))]*)
+and get_vars_func_def func = List.concat [(_id func.returns);(List.concat (List.map get_vars_var_def func.formals));(List.concat(List.map get_vars_stmt func.body))]
+
+let rec referenced_variables stmt =	get_vars_stmt stmt 
 
 let rec free_variables = function
 	| [] -> []
 	| [stmt] -> get_vars_stmt stmt
-	| hd::tl -> (get_vars_stmt hd) @ (free_variables tl)
+	| hd::tl -> (referenced_variables hd) @ (free_variables tl)
+	
--- ray/get_free_vars.ml
+++ ray/get_free_vars.ml
@@ -1,7 +1,7 @@
 open Ast
 
-let _ =
+let _ =  
 	let func = List.hd (Debug.get_example_longest_body "Multi" "Collection") in
 	let stmts = func.body in
 	let free_vars = Free_vars.free_variables stmts in
-	print_string (String.concat "\n" free_vars); print_newline()
+	print_string (String.concat "\n" free_vars); print_newline() 
