ef0c1631df63ba86325af6aa182290a613bf98b8
Matthew Maycock <ummaycoc@gmail.com>
2013-11-30 22:25:26 -0500

    Changed Free_vars.ml to FreeVariables -- more inline with previous naming conventions

--- ray/freevars.ml
+++ ray/freevars.ml
@@ -3,5 +3,5 @@ open Ast
 let _ =  
 	let func = List.hd (Debug.get_example_longest_body "Multi" "FreeVarTest") in
 	let stmts = func.body in
-	let free_vars = Free_vars.free_variables stmts in
+	let free_vars = FreeVariables.free_variables stmts in
 	print_string (String.concat "\n" free_vars); print_newline() 
