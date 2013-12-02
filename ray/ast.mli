4aff14b3c0ae7ea502b180441378a935b500a289
Ben Caimano <bcaimano@gmail.com>
2013-12-01 19:00:01 -0500

    Learning a bit of the ins and outs of ocamldoc

--- ray/ast.mli
+++ ray/ast.mli
@@ -1,14 +1,36 @@
+(**
+    The abstract syntax tree for Gamma
+*)
+
+(**
+    The four literal classes of Gamma
+    * Int - Integer
+    * Float - Floating-point number
+    * String - A sequence of characters
+    * Bool - a boolean value of either true or false
+*)
 type lit =
     Int of int
   | Float of float
   | String of string
   | Bool of bool
 
+(** The binary arithmatic operators *)
 type arith = Add | Sub | Prod | Quot | Div | Mod | Neg | Pow
+
+(** The binary comparison operators *)
 type numtest = Eq | Neq | Less | Grtr | Leq | Geq
+
+(** The binary boolean operators *)
 type combtest = And | Or | Nand | Nor | Xor | Not
+
+(** All three sets of binary operators *)
 type op = Arithmetic of arith | NumTest of numtest | CombTest of combtest
 
+(**
+    The set expressions:
+        This
+*)
 type expr =
     This
   | Null
@@ -24,7 +46,9 @@ type expr =
   | Binop of expr * op * expr (* x + y *)
   | Refine of string * expr list * string option
   | Refinable of string (* refinable *)
+(** The basic variable definition, a type and an id*)
 and var_def = string * string  (* Oh typing, you pain in the ass, add a int for array *)
+(** The basic statements: Variable declarations, control statements, assignments, return statements, and super class expressions *)
 and stmt =
     Decl of var_def * expr option
   | If of (expr option * stmt list) list
@@ -32,22 +56,26 @@ and stmt =
   | Expr of expr
   | Return of expr option
   | Super of expr list
-(* where an item is defined *)
+
+(** We have four different kinds of callable code blocks:
+    - main: only has formals, body (name / static / host known)
+    - init: only has formals, body (name / static / host known)
+    - refine: has host, name, formals, body (static known)
+    - method: has name, formals, body (host / static known)
+*)
+
+(** Three access levels, the refinements, and the main function *)
 and class_section = Publics | Protects | Privates | Refines | Mains
-(* we have four different kinds of callable code blocks:
- *  main: only has formals, body (name / static / host known)
- *  init: only has formals, body (name / static / host known)
- *  refine: has host, name, formals, body (static known)
- *  method: has name, formals, body (host / static known)
- *)
+
+(** The basic function definition *)
 and func_def = {
-  returns : string option;
-  host    : string option;
-  name    : string;
-  static  : bool;
-  formals : var_def list;
-  body    : stmt list;
-  section : class_section;  (* Makes things easier later *)
+  returns : string option;  (** A return type *)
+  host    : string option;  (** A host class *)
+  name    : string;         (** The function name *)
+  static  : bool;           (** If the function is static *)
+  formals : var_def list;   (** A list of all formal parameters of the function *)
+  body    : stmt list;      (** A list of statements that form the function body *)
+  section : class_section;  (** A semenantic tag of the class section in which the function lives *)
 }
 
 (* A member is either a variable or some sort of function *)
