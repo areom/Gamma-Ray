45f8a786a03cde3d99fd57c3b82075f8416ba528
ArthySundaram <arthysundaram@gmail.com>
2013-12-02 18:47:57 -0500

    First step in type checking

--- ray/Build.ml
+++ ray/Build.ml
@@ -3,11 +3,35 @@ open StringModules
 
 let env = StringMap.empty
 
+
+let get_type exp =
+	match exp with
+		Literal(l) ->
+			match l with ->
+				Int(_) -> Int
+			|	Float(_) -> Float
+			|	String(_) -> String
+			|	Bool(_) -> Boolean
+				
+	|       Binop(e1,x,e2) ->
+				let t1 = get_type e1 and
+				let t2 = get_type e2
+				if(t1 = Int and t2 = Int) then
+					Int
+				else
+					raise "Required Int"
+
+
+let annotate_expr expr = (expr , get_type expr)	
+
+let annotate_exprlist elist = List.map annotate_expr elist
+
+
 let rec attach_bindings stmts env =
 
     let build_ifstmt iflist env=
 	
-	let build_block env (condn, slist) = (condn, (attach_bindings slist env))
+	let build_block env (expr, slist) = (annotate_expr expr, (attach_bindings slist env))
 
 	in
 	Sast.If( List.map (build_block env) iflist, env)
@@ -17,12 +41,13 @@ let rec attach_bindings stmts env =
     let build_env (output, env) stmt =
 
 	match stmt with
-		| Ast.While(expr, slist)     	->  (Sast.While((expr, attach_bindings slist env), env)::output, env)
+		| Ast.While(expr, slist)     	->  (Sast.While((annotate_expr expr, attach_bindings slist env), env)::output, env)
  		| Ast.If (iflist)            	->  ((build_ifstmt iflist env)::output, env)
-		| Ast.Decl((vtype,vname), opt) 	->  (Sast.Decl((vtype, vname), opt, env)::output, (StringMap.add vname (vtype,Local) env))
-  		| Ast.Expr(exp) 		-> (Sast.Expr(exp, env)::output, env)
-		| Ast.Return(exp) 		-> (Sast.Return(exp, env)::output, env)
-	        | Ast.Super(elist) 		-> (Sast.Super(elist,env)::output, env)
+		| Ast.Decl((vtype,vname),opt_expr)->  (Sast.Decl((vtype, vname), annotate_optexpr opt_expr, env)::output, 
+								(StringMap.add vname (vtype,Local) env))
+  		| Ast.Expr(expr) 		-> (Sast.Expr(annotate_expr expr, env)::output, env)
+		| Ast.Return(expr) 		-> (Sast.Return(annotate_expr expr, env)::output, env)
+	        | Ast.Super(expr_list) 		-> (Sast.Super(annotate_exprlist expr_list,env)::output, env)
 
     in (List.rev (fst(List.fold_left build_env ([],env) stmts)))
 
