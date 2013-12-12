f97506d7a4a51e34e2445e1c023b430c11078672
Weiyuan Li <liwy34@gmail.com>
2013-12-12 17:05:38 -0500

    Aligned the arrows...

--- ray/GenCast.ml
+++ ray/GenCast.ml
@@ -7,15 +7,15 @@ let rec sast_to_castexpr sast_expr =
 and c_expr_detail sastexp =
 
     match sastexp with
-      Sast.This              ->    Cast.This
-    | Sast.Null              ->    Cast.Null
-    | Sast.Id(vname)         ->    Cast.Id(vname)
-    | Sast.Literal(lit)      ->    Cast.Literal(lit)
-    | Sast.Assign(e1, e2)    ->    Cast.Assign( sast_to_castexpr e1, sast_to_castexpr e2)
-    | Sast.Deref(e1, e2)     ->    Cast.Deref(sast_to_castexpr e1, sast_to_castexpr e2)
-    | Sast.Unop(op, expr)    ->    Cast.Unop(op, sast_to_castexpr expr)
-    | Sast.Binop(e1, op, e2) ->    Cast.Binop(sast_to_castexpr e1, op, sast_to_castexpr e2)
-    | _    ->    Cast.Null (* To avoid warning*)
+      Sast.This              -> Cast.This
+    | Sast.Null              -> Cast.Null
+    | Sast.Id(vname)         -> Cast.Id(vname)
+    | Sast.Literal(lit)      -> Cast.Literal(lit)
+    | Sast.Assign(e1, e2)    -> Cast.Assign( sast_to_castexpr e1, sast_to_castexpr e2)
+    | Sast.Deref(e1, e2)     -> Cast.Deref(sast_to_castexpr e1, sast_to_castexpr e2)
+    | Sast.Unop(op, expr)    -> Cast.Unop(op, sast_to_castexpr expr)
+    | Sast.Binop(e1, op, e2) -> Cast.Binop(sast_to_castexpr e1, op, sast_to_castexpr e2)
+    | _                      -> Cast.Null (* To avoid warning*)
 
 
 (*Convert the statement list by invoking cstmt on each of the sast stmt*)
@@ -27,21 +27,20 @@ and cstmt sstmt =
     let getoptexpr optexpr = 
         match optexpr with
           Some exp -> Some(sast_to_castexpr exp)
-        | None    -> None
+        | None     -> None
     in
 					
 		let rec getiflist iflist =
 		    match iflist with
-				  []                    ->    []
-				| [(optexpr, slist)]    ->    [(getoptexpr optexpr, cstmtlist slist)]
-				| (optexpr, slist)::tl  ->    (getoptexpr optexpr, cstmtlist slist):: getiflist tl    
+				  []                   -> []
+				| [(optexpr, slist)]   -> [(getoptexpr optexpr, cstmtlist slist)]
+				| (optexpr, slist)::tl -> (getoptexpr optexpr, cstmtlist slist):: getiflist tl    
 		in
-				  
 
     match sstmt with
       Sast.Decl(var_def, optexpr, env) -> Cast.Decl(var_def, getoptexpr optexpr, env)
     | Sast.If(iflist, env)             -> Cast.If(getiflist iflist, env)
     | Sast.While(expr, sstmtlist, env) -> Cast.While(sast_to_castexpr expr, cstmtlist sstmtlist, env)
     | Sast.Expr(exp, env)              -> Cast.Expr(sast_to_castexpr exp, env)
-    | Sast.Return(optexpr,env)        -> Cast.Return(getoptexpr optexpr, env)
-    | _  -> raise (Failure "Yet to implement all statement")
+    | Sast.Return(optexpr,env)         -> Cast.Return(getoptexpr optexpr, env)
+    | _                                -> raise (Failure "Yet to implement all statement")
