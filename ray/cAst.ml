70be61383b684aeb08aa86e6265921894da74de7
ArthySundaram <arthysundaram@gmail.com>
2013-12-12 00:09:19 -0500

    Modified cast

--- ray/cAst.ml
+++ ray/cAst.ml
@@ -2,52 +2,56 @@ type varkind = Instance | Local
 
 type environment = (string * varkind) Map.Make(String).t
 
-type expr_detail =
+type cexpr_detail =
     | This
     | Null
     | Id of string
-    | NewObj of string * expr list
+(*    | NewObj of string * expr list
     | Anonymous of string * Ast.expr list * Ast.func_def list (* Evaluation is delayed *)
+*)
     | Literal of Ast.lit
-    | Assign of expr * expr  (* memory := data -- whether memory is good is a semantic issue *)
-    | Deref of expr * expr (* road[pavement] *)
+    | Assign of cexpr * cexpr  (* memory := data -- whether memory is good is a semantic issue *)
+    | Deref of cexpr * cexpr (* road[pavement] *)
+(*
     | Field of expr * string (* road.pavement *)
     | Invoc of expr * string * expr list (* receiver.method(args) *)
-    | Unop of Ast.op * expr (* !x *)
-    | Binop of expr * Ast.op * expr (* x + y *)
-    | Refine of string * expr list * string option
+*) 
+   | Unop of Ast.op * cexpr (* !x *)
+    | Binop of cexpr * Ast.op * cexpr (* x + y *)
+(* 
+   | Refine of string * expr list * string option
     | Refinable of string (* refinable *)
-
+*)
 and  
 
-cexpr = string * expr_detail
+cexpr = string * cexpr_detail
 
 and 
   
 cvar_def = (string * string)
 
 and cstmt =
-    | Decl of var_def * expr option * environment
-    | If of (expr option * sstmt list) list * environment
-    | While of expr * sstmt list * environment
-    | Expr of expr * environment
-    | Return of expr option * environment
+    | Decl of cvar_def * cexpr option * environment
+    | If of (cexpr option * cstmt list) list * environment
+    | While of cexpr * cstmt list * environment
+    | Expr of cexpr * environment
+    | Return of cexpr option * environment
 (*    | Super of expr list * environment*)
-
+(*
 
 and cfunc_def = {
     returns : string option;
     host    : string option;
     name    : string;
     static  : bool;
-    formals : var_def list;
+    formals : cvar_def list;
     body    : sstmt list;
     section : Ast.class_section;  (* Makes things easier later *)
     inklass : string;
 }
 
 (* A member is either a variable or some sort of function *)
-type member_def = VarMem of var_def | MethodMem of func_def | InitMem of func_def
+type member_def = VarMem of cvar_def | MethodMem of func_def | InitMem of func_def
 
 (* Things that can go in a class *)
 type class_sections_def = {
@@ -66,3 +70,4 @@ type class_def = {
 }
 
 type program = class_def list
+*)
