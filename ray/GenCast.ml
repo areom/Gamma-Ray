2c98763972733c679d5e8ea41e4546b9ac98b242
Weiyuan Li <liwy34@gmail.com>
2013-12-12 16:58:03 -0500

    Add getiflist in GenCast.ml and a bit styling change.

--- ray/GenCast.ml
+++ ray/GenCast.ml
@@ -12,13 +12,13 @@ and c_expr_detail sastexp =
     | Sast.Id(vname)         ->    Cast.Id(vname)
     | Sast.Literal(lit)      ->    Cast.Literal(lit)
     | Sast.Assign(e1, e2)    ->    Cast.Assign( sast_to_castexpr e1, sast_to_castexpr e2)
+    | Sast.Deref(e1, e2)     ->    Cast.Deref(sast_to_castexpr e1, sast_to_castexpr e2)
     | Sast.Unop(op, expr)    ->    Cast.Unop(op, sast_to_castexpr expr)
     | Sast.Binop(e1, op, e2) ->    Cast.Binop(sast_to_castexpr e1, op, sast_to_castexpr e2)
-    | Sast.Deref(e1, e2)     ->    Cast.Deref(sast_to_castexpr e1, sast_to_castexpr e2)
     | _    ->    Cast.Null (* To avoid warning*)
 
 
-(*Conver the statement list by invoking cstmt on each of the sast stmt*)
+(*Convert the statement list by invoking cstmt on each of the sast stmt*)
 let rec cstmtlist slist =  List.map cstmt slist
 
 (*convert sast statement to c statements*)
@@ -29,10 +29,18 @@ and cstmt sstmt =
           Some exp -> Some(sast_to_castexpr exp)
         | None    -> None
     in
+					
+		let rec getiflist iflist =
+		    match iflist with
+				  []                    ->    []
+				| [(optexpr, slist)]    ->    [(getoptexpr optexpr, cstmtlist slist)]
+				| (optexpr, slist)::tl  ->    (getoptexpr optexpr, cstmtlist slist):: getiflist tl    
+		in
+				  
 
     match sstmt with
       Sast.Decl(var_def, optexpr, env) -> Cast.Decl(var_def, getoptexpr optexpr, env)
-    (*| Sast.If(iflist, env)             -> Cast.If(getiflist iflist, env)*)
+    | Sast.If(iflist, env)             -> Cast.If(getiflist iflist, env)
     | Sast.While(expr, sstmtlist, env) -> Cast.While(sast_to_castexpr expr, cstmtlist sstmtlist, env)
     | Sast.Expr(exp, env)              -> Cast.Expr(sast_to_castexpr exp, env)
     | Sast.Return(optexpr,env)        -> Cast.Return(getoptexpr optexpr, env)
