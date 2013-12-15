11d3ea229c26b953c1eb9a65109814b91431f12a
Matthew Maycock <ummaycoc@gmail.com>
2013-12-15 16:42:47 -0500

    Added documentation to show up in generated doc

--- ray/GlobalData.mli
+++ ray/GlobalData.mli
@@ -5,65 +5,17 @@ open StringModules
 
 (** A full class record table as a type *)
 type class_data = {
-    (** A set of known class names *)
-    known : StringSet.t;
-
-    (** A map that goes from class names to to class definition records *)
-    classes : class_def lookup_map;
-
-    (**
-        A map that goes from class names to parents --
-        everything should have a parent except Object
-      *)
-    parents : string lookup_map;
-
-    (** A map that goes from class names to a list of children *)
-    children : (string list) lookup_map;
-
-    (**
-        A table [map -> map -> value]; primary key is the class name;
-        secondary key is variable name; result is a pair: the section
-        variable was declared in and its type
-      *)
-    variables : (class_section * string) lookup_table;
-
-    (**
-        A table [map -> map -> value]; primary key is the class name;
-        secondary key is the method name; result is a list of functions
-        that have the given name in the given class
-      *)
-    methods : (func_def list) lookup_table;
-
-    (**
-        A table [map -> map -> value]; primary key is the class name;
-        secondary key is the refinement `host.refinement'; the result is
-        a list of functions that have the given name in the given class
-      *)
-    refines : (func_def list) lookup_table;
-
-    (** A map from class names to main functions *)
-    mains : func_def lookup_map;
-
-    (**
-        A map from class names to the list of ancestors -- the list runs
-        from the given class back to Object in the expected fashion
-      *)
-    ancestors : (string list) lookup_map;
-
-    (**
-        A table [map -> map -> value]; primary key is subtype; secondary
-        key is the ancestor; result is the positive integer distance that
-        represents how many hops along the class tree are between the two
-        classes. Distance from a type to itself is 0.
-      *)
-    distance : int lookup_table;
-
-    (**
-        A table [map -> map -> value]; primary key is a class name; the
-        secondary key is the refinement's full name (host.name); the value
-        is the uid of the dispatcher for this refinement.
-      *)
-    dispatcher : string lookup_table;
+    known : StringSet.t; (** Set of known class names *)
+    classes : class_def lookup_map; (** class name -> class def map *)
+    parents : string lookup_map; (** class name -> parent name map *)
+    children : (string list) lookup_map; (** class name -> children list map *)
+    variables : (class_section * string) lookup_table; (** class name -> var name -> (section, type) map *)
+    methods : (func_def list) lookup_table; (** class name -> method name -> func_def list map *)
+    refines : (func_def list) lookup_table; (** class name -> host.refinement -> func_def list map *)
+    mains : func_def lookup_map; (** class name -> main map *)
+    ancestors : (string list) lookup_map; (** class name -> ancestor list (given to Object) *)
+    distance : int lookup_table; (** subtype -> supertype -> # hops map *)
+    dispatcher : string lookup_table; (** class -> host.refinement -> uid map *)
 }
 
 (**
