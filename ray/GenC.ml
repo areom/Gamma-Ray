6d8fa1dfea40bb294259411a2395b0faf0e5f18e
ArthySundaram <arthysundaram@gmail.com>
2013-12-16 00:12:25 -0500

    Whooo code gen

--- ray/GenC.ml
+++ ray/GenC.ml
@@ -1,35 +1,53 @@
 open Cast
- 
+
+let lit_to_str lit = 
+    match lit with
+      Ast.Int(i) -> string_of_int i
+    | Ast.Float(f) -> string_of_float f
+    | Ast.String(s) -> s
+    | Ast.Bool(b) ->if b = true then "1" else "0"
+
+
+let stringify_unop op lop = 
+    match op with
+      Ast.Arithmetic(Ast.Neg) -> "-"^lop
+    | Ast.CombTest(Ast.Not)   -> "!"^lop
+    | _   -> raise (Failure "Unknown operator")
+
 let stringify_arith op lop rop =
     match op with
-      Add  -> " + "
-    | Sub  -> " - "
-    | Prod -> " * "
-    | Div  -> " / "
-    | Mod  -> " % "
-    | Neg  -> " may be wrong - ? it's an unop"
-    | Pow  -> " ^ "
+      Ast.Add  -> lop^" + "^rop
+    | Ast.Sub  -> lop^" - "^rop
+    | Ast.Prod -> lop^" * "^rop
+    | Ast.Div  -> lop^" / "^rop
+    | Ast.Mod  -> lop^" % "^rop
+    | Ast.Neg  ->  raise(Failure "Unary operator")
+    | Ast.Pow  -> lop^" ^ "^rop
+    | Ast.Quot ->  raise(Failure "Unused Quot remove it")
 
 let stringify_numtest op lop rop =
     match op with
-      Eq   -> " == "
-    | Neq  -> " != "
-    | Less -> " < "
-    | Grtr -> " > "
-    | Leq  -> " <= "
-    | Geq  -> " >= "
+      Ast.Eq   -> lop^" == "^rop
+    | Ast.Neq  -> lop^" != "^rop
+    | Ast.Less -> lop^" < "^rop
+    | Ast.Grtr -> lop^" > "^rop
+    | Ast.Leq  -> lop^" <= "^rop
+    | Ast.Geq  -> lop^" >= "^rop
 
 let stringify_combtest op lop rop =
-    match combtest with
-      And  -> " && "
-    | Or   -> " || "
-    | Nand -> ""
+    match op with
+      Ast.And  -> lop^" && "^rop
+    | Ast.Or   -> lop^" || "^rop
+    | Ast.Nand -> "!( "^lop^" && "^rop^" )"
+    | Ast.Nor  -> "!( "^lop^" || "^rop^" )"
+    | Ast.Xor  -> "!( "^lop^" == "^rop^" )" 
+    | Ast.Not  -> raise(Failure "Unary operator")
 
 let stringify_binop op  lop rop=
     match op with
-      Arithmetic(arith)  -> stringify_arith arith lop rop
-    | NumTest(numtest)   -> stringify_numtest numtest lop rop
-    | CombTest(combtest) -> stringify_combtest combtest lop rop
+      Ast.Arithmetic(arith)  -> stringify_arith arith lop rop
+    | Ast.NumTest(numtest)   -> stringify_numtest numtest lop rop
+    | Ast.CombTest(combtest) -> stringify_combtest combtest lop rop
 
 let stringify_list stmtlist =
    String.concat "\n" stmtlist
@@ -42,14 +60,14 @@ and exprdetail_to_cstr castexpr_detail =
     | This          -> "We wont have a this right?"
     | Null          -> "NULL"
     | Id(vname)     -> vname
-    | NewObj(classname, fname, args) -> " "
-    | Literal(lit)  -> "lit_to_str lit"
+    | NewObj(classname, fname, args) -> "NEWOBJ TBD"
+    | Literal(lit)  -> lit_to_str lit
     | Assign(memory, data) -> (expr_to_cstr memory)^" = "^(expr_to_cstr data)
     | Deref(carray, index) -> (expr_to_cstr carray)^"["^(expr_to_cstr index)^"]"
     | Field(obj, fieldname) -> (expr_to_cstr obj)^"."^(fieldname) 
-    | Invoc(recvr, fname, args) ->  " Invoc"
-    | Unop(op, expr) -> "Unop" 
-    | Binop(lop, op, rop) -> stringify_binop op (expr_to_cstr lop) (expr_to_cstr rop)
+    | Invoc(recvr, fname, args) ->  "IMPLEMENT INVOC - TBD"
+    | Unop(op, expr) -> stringify_unop op (expr_to_cstr expr) 
+    | Binop(lop, op, rop) -> "( "^(stringify_binop op (expr_to_cstr lop) (expr_to_cstr rop))^" )"
 
 let rec cast_to_cstmtlist stmtlist = stringify_list  (List.map cast_to_c_stmt stmtlist)
 
