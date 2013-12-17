248bb2bf0eed94dadfbcc092de7acdc24f5c67f5
Ben Caimano <bcaimano@gmail.com>
2013-12-17 03:40:21 -0500

    Put in header file generation

--- ray/GenC.ml
+++ ray/GenC.ml
@@ -52,6 +52,7 @@ let stringify_binop op  lop rop=
 let stringify_list stmtlist =
    String.concat "\n" stmtlist
 
+
 let rec expr_to_cstr (exptype, expr_detail) = exprdetail_to_cstr expr_detail
 
 and exprdetail_to_cstr castexpr_detail =
@@ -69,21 +70,31 @@ and exprdetail_to_cstr castexpr_detail =
     | Unop(op, expr) -> stringify_unop op (expr_to_cstr expr) 
     | Binop(lop, op, rop) -> "( "^(stringify_binop op (expr_to_cstr lop) (expr_to_cstr rop))^" )"
 
-let rec cast_to_cstmtlist stmtlist = stringify_list  (List.map cast_to_c_stmt stmtlist)
 
 and vdecl_to_cstr (vtype, vname) = vtype^" "^vname^";\n"
 
+(**
+    Take a list of cast_stmts and return a body of c statements
+    @param stmtlist A list of statements
+    @return A body of c statements
+*)
+let rec cast_to_cstmtlist stmtlist = stringify_list  (List.map cast_to_c_stmt stmtlist)
+
 and ifstmt_to_str level (ifexpr, body) = 
-
     match ifexpr with
-      Some(ifpred) ->  if level <> 0 then 
-                           "elseif( "^(expr_to_cstr ifpred)^" ) {\n"^(cast_to_cstmtlist body)^"\n}\n"
-                       else
-                           "if( "^(expr_to_cstr ifpred)^" ) {\n"^(cast_to_cstmtlist body)^"\n}\n "
+    | Some(ifpred)   ->  if level <> 0 then 
+       "elseif( " ^ (expr_to_cstr ifpred) ^" ) {\n"
+        ^(cast_to_cstmtlist body)^"\n}\n"
+        else
+        "if( "^(expr_to_cstr ifpred)^" ) {\n"^(cast_to_cstmtlist body)^"\n}\n "
+    | None           ->  "else {\n"^(cast_to_cstmtlist body)^" }\n"
 
-    | None   ->       "else {\n"^(cast_to_cstmtlist body)^" }\n"
 
-
+(**
+    Output a statement in c
+    @param caststmt A statement in C-Ast form
+    @returm A c statement
+*)
 and   cast_to_c_stmt caststmt = 
 
     match caststmt with
@@ -96,4 +107,79 @@ and   cast_to_c_stmt caststmt =
     | _ -> "Yet to implement this statement" 
 
 
+(**
+    Take a C-Ast and return a program in c
+    @param cast_cdefs A list of class definitions
+    @param cast_cfuncs A list of function defintions
+    @param cast_mains A list of main method uids
+    @return A program in c
+*)
+(**
+let cast_to_c (cast_cdefs,cast_cfuncs,cast_mains) =
+    (cast_to_c_structs cast_cdefs) ^
+    (cast_to_c_funcs cast_cfuns) ^
+    (cast_to_c_mains cast_mains)*)
 
+(** 
+   Take a class definition and return code consisting of struct types 
+   @param cast_cdefs A list of class definitions
+   @return A set of type definitions
+*)
+let cast_to_h_types(cast_cdefs) =
+    let generic_class_struct =
+        "typedef struct{\n"^
+        "  int class_data;\n"^
+        "  int var_data;\n"^
+        "  int refine_data;\n"^
+        "} gen_class;\n\n"
+    in
+    (* I'm a little unsure if there are more data points to add here.
+    Maybe this could be replaced with class_name alone? *)
+    let class_details_struct =
+        "typedef struct {\n" ^
+        "  char* class_name;\n" ^
+        "} class_details;"
+    in
+    (**
+        Classes have three important component structs: class data,
+        refinement data, and variables. They get tied together by a generic
+        class struct
+    *)
+    let cast_to_h_class cast_cdef =
+        (** Put out a pointer for each variable as part of a struct *)
+        let cast_to_h_var_set =
+            "typedef struct {\n" ^
+            (String.concat "\n" (List.map
+                (fun (type_name,var_name) -> "  gen_class* v_"^var_name^";")
+                cast_cdef.variables)) ^
+            "} cv_" ^ (List.hd cast_cdef.klass) ^ ";"
+        in
+        (** Put out a pointer for each refinement function as part of a struct *)
+        let cast_to_h_ref_set =
+            "typedef struct {\n"^
+            (String.concat "\b" (List.map
+                (fun ref_name -> "  int f_"^ref_name^";\n")
+                cast_cdef.refines)) ^
+            "} cr_" ^ (List.hd cast_cdef.klass) ^ ";"
+        in
+        cast_to_h_var_set ^ "\n\n"
+        ^ cast_to_h_ref_set
+    in
+    generic_class_struct ^ class_details_struct
+    ^ (String.concat "\n\n" (List.map cast_to_h_class cast_cdefs))
+
+(**
+    Build code for function header definitions
+    @param cfuncs A list of function definintions
+    @return A string of code containing function header
+*)
+let cast_to_h_funcs cfuncs =
+    let cast_to_h_func cfunc =
+        let cast_to_h_var (type_name,var_name) =
+            "gen_class * v_"^var_name
+        in
+        "gen_class * f_"^cfunc.uid^"(gen_class * host,"
+        ^(String.concat "," (List.map cast_to_h_var cfunc.formals))
+        ^");"
+    in
+    (String.concat "\n\n" (List.map cast_to_h_func cfuncs))
