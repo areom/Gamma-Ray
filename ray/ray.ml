775c456c3ffcfc0b87fba5e3685407e549deb153
Ben Caimano <bcaimano@gmail.com>
2013-12-17 06:55:44 -0500

    Moved the file around a little

--- ray/ray.ml
+++ ray/ray.ml
@@ -1,29 +1,34 @@
 open Printf
 open Util
-open Classinfo
 
-let source_file = open_in Sys.argv.(1)
+let output_string whatever =
+    print_string whatever;
+    print_newline()
 
-let output_string str =
-    let _ = print_string str in
-    print_newline ()
+let load_file filename =
+    if Sys.file_exists filename
+        then open_in filename
+        else raise Not_found;;
+
+let source_file = load_file Sys.argv.(1)
 
 let source_cast =
     let tokens = Inspector.from_channel source_file in
-    let (_) = output_string "Loaded tokens" in
+    close_in source_file;
+    output_string "Loaded tokens";
     let ast = Parser.cdecls (WhiteSpace.lextoks tokens) (Lexing.from_string "") in
-    let (_) = output_string "Formed AST" in
+    output_string "Formed AST";
     let klass_data_test = KlassData.build_class_data ast in
     (** May the legions of hell descend on this monadism. *)
     let klass_data = match klass_data_test with
         | Left(x) -> x
-        | Right(issue) -> Printf.fprintf stderr "%s\n" (errstr issue); exit(1)
+        | Right(issue) -> Printf.fprintf stderr "%s\n" ("Really need to move the error function out of classinfo"); exit(1)
     in
-    let (_) = output_string "Tabulated class data" in
+    output_string "Tabulated class data";
     let sast = BuildSast.ast_to_sast klass_data ast in
-    let (_) = output_string "Transformed from AST to SAST" in
+    output_string "Transformed from AST to SAST";
     let cast = GenCast.sast_to_cast klass_data sast in
-    let (_) = output_string "Transformed from SAST to CAST" in
+    output_string "Transformed from SAST to CAST";
     cast
 
 let output_h = open_out (Sys.argv.(2) ^ ".h")
@@ -37,4 +42,4 @@ let print_header =
 let print_source =
     let gen_source = GenC.cast_to_c source_cast in
     fprintf output_c "%s" gen_source;
-    close_out output_c
+    close_out output_c 
