8bd760b29c0b22be3880788eb91d74b9c1ec87f2
Ben Caimano <bcaimano@gmail.com>
2013-12-17 06:07:52 -0500

    Added status messages somewhat

--- ray/build.ml
+++ ray/build.ml
@@ -4,17 +4,26 @@ open Classinfo
 
 let source_file = open_in Sys.argv.(1)
 
+let output_string str =
+    let _ = print_string str in
+    print_newline ()
+
 let source_cast =
     let tokens = Inspector.from_channel source_file in
+    let (_) = output_string "Loaded tokens" in
     let ast = Parser.cdecls (WhiteSpace.lextoks tokens) (Lexing.from_string "") in
+    let (_) = output_string "Formed AST" in
     let klass_data_test = KlassData.build_class_data ast in
     (** May the legions of hell descend on this monadism. *)
     let klass_data = match klass_data_test with
         | Left(x) -> x
         | Right(issue) -> Printf.fprintf stderr "%s\n" (errstr issue); exit(1)
     in
+    let (_) = output_string "Tabulated class data" in
     let sast = BuildSast.ast_to_sast klass_data ast in
+    let (_) = output_string "Transformed from AST to SAST" in
     let cast = GenCast.sast_to_cast klass_data sast in
+    let (_) = output_string "Transformed from SAST to CAST" in
     cast
 
 let output_h = open_out (Sys.argv.(2) ^ ".h")
