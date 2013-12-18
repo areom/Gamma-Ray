1847d6f61f8ae648edac2ef3edf3a801fdf1dec2
Matthew Maycock <ummaycoc@gmail.com>
2013-12-17 23:07:21 -0500

    Changed to use new workflow

--- ray/ray.ml
+++ ray/ray.ml
@@ -10,36 +10,21 @@ let load_file filename =
         then open_in filename
         else raise Not_found;;
 
-let source_file = load_file Sys.argv.(1)
+let with_file f file =
+    let input = load_file file in
+    let result = f input in
+    close_in input;
+    result
+
+let get_data ast = match KlassData.build_class_data ast with
+    | Left(data) -> data
+    | Right(issue) -> Printf.fprintf stderr "%s" (KlassData.errstr issue); exit(1)
 
 let source_cast =
-    let tokens = Inspector.from_channel source_file in
-    close_in source_file;
-    output_string "Loaded tokens";
+    let tokens = with_file Inspector.from_channel Sys.argv.(1) in
     let ast = Parser.cdecls (WhiteSpace.lextoks tokens) (Lexing.from_string "") in
-    output_string "Formed AST";
-    let klass_data_test = KlassData.build_class_data ast in
-    (** May the legions of hell descend on this monadism. *)
-    let klass_data = match klass_data_test with
-        | Left(x) -> x
-        | Right(issue) -> Printf.fprintf stderr "%s\n" ("Really need to move the error function out of classinfo"); exit(1)
-    in
-    output_string "Tabulated class data";
+    let klass_data = get_data ast in
     let sast = BuildSast.ast_to_sast klass_data ast in
-    output_string "Transformed from AST to SAST";
-    let cast = GenCast.sast_to_cast klass_data sast in
-    output_string "Transformed from SAST to CAST";
-    cast
+    GenCast.sast_to_cast klass_data sast
 
-let output_h = open_out (Sys.argv.(2) ^ ".h")
-let output_c = open_out (Sys.argv.(2) ^ ".c")
-
-let print_header =
-    let gen_header = GenC.cast_to_h source_cast in
-    fprintf output_h "%s" gen_header;
-    close_out output_h
-
-let print_source =
-    let gen_source = GenC.cast_to_c source_cast in
-    fprintf output_c "%s" gen_source;
-    close_out output_c
+let _ = GenC.cast_to_c source_cast stdout
