c191421fe44eac6a6eab499754ab0283c150881e
Matthew Maycock <ummaycoc@gmail.com>
2013-12-21 11:54:34 -0500

    Now we can just pass - or -- to classinfo to get builtin info

--- ray/classinfo.ml
+++ ray/classinfo.ml
@@ -1,13 +1,31 @@
 open Util
 
-let _ =
-    try
-        Printexc.record_backtrace true;
-        let tokens = Inspector.from_channel stdin in
-        let classes = Parser.cdecls (WhiteSpace.lextoks tokens) (Lexing.from_string "") in
-        let builder = if (Array.length Sys.argv = 1) then KlassData.build_class_data else KlassData.build_class_data_test in
-        match builder classes with
-            | Left(data) -> KlassData.print_class_data data; exit(0)
-            | Right(issue) -> Printf.fprintf stderr "%s\n" (KlassData.errstr issue); exit(1)
-    with _ ->
-        Printexc.print_backtrace stderr
+let show_classes builder classes = match builder classes with
+    | Left(data) -> KlassData.print_class_data data; exit(0)
+    | Right(issue) -> Printf.fprintf stderr "%s\n" (KlassData.errstr issue); exit(1)
+
+let from_input builder =
+    let tokens = Inspector.from_channel stdin in
+    let classes = Parser.cdecls (WhiteSpace.lextoks tokens) (Lexing.from_string "") in
+    show_classes builder classes
+let from_basic builder = show_classes builder []
+
+let basic_info_test () = from_basic KlassData.build_class_data_test
+let basic_info () = from_basic KlassData.build_class_data
+
+let test_info () = from_input KlassData.build_class_data_test
+let normal_info () = from_input KlassData.build_class_data    
+
+let exec name func = Printf.printf "Executing mode %s\n" name; flush stdout; func ()
+
+let _ = try
+    Printexc.record_backtrace true;
+    match Array.to_list Sys.argv with
+        | []     -> raise(Failure("Not even program name given as argument."))
+        | [_]    -> exec "Normal Info" normal_info
+        | _::arg::_ -> match arg with
+        | "-"    -> exec "Basic Info"  basic_info
+        | "--"   -> exec "Basic Test"  basic_info_test
+        | _      -> exec "Test Info"   test_info
+with _ ->
+    Printexc.print_backtrace stderr
