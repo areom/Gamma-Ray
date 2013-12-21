929be50991d08e9b1784b30cc13eab375c6fa062
Weiyuan Li <liwy34@gmail.com>
2013-12-21 17:24:54 -0500

    Header files

---
+++ ray/headers/gamma-preamble.h
@@ -0,0 +1,21 @@
+#define LIT_INT(lit_int)
+#define LIT_FLOAT(lit_flt)
+#define LIT_STRING(lit_str)
+#define LIT_BOOL(lit_bool)
+
+#define ADD_INT_INT(l, r)
+#define ADD_FLOAT_FLOAT(l, r)
+#define SUB_INT_INT(l, r)
+#define SUB_FLOAT_FLOAT(l, r)
+#define PROD_INT_INT(l, r)
+#define PROD_FLOAT_FLOAT(l, r)
+#define DIV_INT_INT(l, r)
+#define DIV_FLOAT_FLOAT(l, r)
+#define MOD_INT_INT(l, r)
+#define POW_INT_INT(l, r)
+#define POW_FLOAT_FLOAT(l, r)
+
+
+#define MakeNew(t_name)
+
+#define BOOL_OF(boolean)
