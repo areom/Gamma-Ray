adece77d67589acbc1274d0586590d402cc9da2f
Matthew Maycock <ummaycoc@gmail.com>
2013-12-22 18:54:56 -0500

    Cats file now, assumes Test is the main to use.

--- ray/run-compiler-test.sh
+++ ray/run-compiler-test.sh
@@ -7,12 +7,13 @@ function errwith {
 
 test "$#" -lt 1 && errwith "Please give a file to test"
 file=$1
-main=$2
 
 test -e "$file" || errwith "File $file does not exist."
 test -f "$file" || errwith "File $file is not a file."
 
-args=()
-test -n "$main" && args+=( "$main" )
-./bin/ray "$file" > ctest/test.c && cd ctest && ./compile && ./a.out "${args[@]}"
+echo "=========================="
+echo "$file"
+cat "$file"
+echo "=========================="
+./bin/ray "$file" > ctest/test.c && cd ctest && ./compile && ./a.out Test
 
