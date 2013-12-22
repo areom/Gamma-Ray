966c2066d37c7b51bdb2a44e0bf6b5f3aba6d4b6
Matthew Maycock <ummaycoc@gmail.com>
2013-12-21 23:04:10 -0500

    Fixed a few things up.

--- ray/headers/gamma-builtin-functions.h
+++ ray/headers/gamma-builtin-functions.h
@@ -1,4 +1,9 @@
 
+/* GLOBAL DATA */
+t_System global_system;
+int obj_counter;
+
+
 /* Magic allocator. DO NOT INVOKE THIS, USE MAKE_NEW(TYPE)
  * where type is not prefixed (i.e. MAKE_NEW(Integer) not
  * MAKE_NEW(t_Integer))
@@ -70,8 +75,8 @@ t_Float *float_init(t_Float *this){
 }
 
 t_Object *object_init(t_Object *this){
-    this->Object = *object_init(&this->Object);
-    this->Object.v_system = system_init(this->Object.v_system);
+    this->Object.v_system = &global_system;
+    this->Object.v_obj_id = ++object_counter;
     return this;
 }
 
@@ -85,7 +90,7 @@ t_String *string_init(t_String *this)
 t_Printer *printer_init(t_Printer *this, t_Boolean *v_stdout)
 {
     this->Object = *object_init(&this->Object);
-    this->Printer.target = BOOL_OF(v_stdout) ? stdout : stderr;
+    this->Printer.target = v_stdout->Boolean.value ? stdout : stderr;
     return this;
 }
 
@@ -96,72 +101,62 @@ t_Scanner *scanner_init(t_Scanner *this)
 }
 
 t_Integer *float_to_i(t_Float *this){
-    t_Integer *new_int = MAKE_NEW(Integer);
-    new_int = integer_init(new_int);
-    new_int->Integer.value = (int)(this->Float.value);
-    return new_int;
+    return integer_value((int)(this->Float.value));
 }
 
 t_Float *integer_to_f(t_Integer *this){
-    t_Float *new_float = MAKE_NEW(Float);
-    new_float = float_init(new_float);
-    new_float->Float.value = (float)(this->Integer.value);
-    return new_float
+    return float_value((double)(this->Integer.value));
 }
 
-t_String *object_get_id(t_Object *this){
+t_Integer *object_get_id(t_Object *this){
     return this->Object.v_obj_id;
 }
-
-/* void system_exit(t_System *this, t_Integer *v_code) */
-/* t_System *system_init(t_System *this) */
-
 
 t_Float *scanner_scan_float(t_Scanner *this)
 {
     double dval;
     fscanf(this->Scanner.source, "%ld", &dval);
-
-    t_Float *new_float = MAKE_NEW(Float);
-    new_float = float_init(new_float);
-    new_float->Float.value = dval;
+    return float_value(dval);
 }
 
 t_Integer *scanner_scan_integer(t_Scanner *this)
 {
     int ival;
     fscanf(this->Scanner.source, "%d", &val);
-
-    t_Integer *new_int = MAKE_NEW(Integer);
-    new_int = integer_init(new_int);
-    new_int->Integer.value = ival;
+    return integer_value(ival);
 }
 
 t_String *scanner_scan_string(t_Scanner *this)
 {
     int ret = -1;
     char *inpstr = NULL;
+    t_String *astring = NULL;
     ret = getline(&inpstr, 0, this->Scanner.source);
     if(ret == -1) {
         fprintf(stderr, "Error in string input\n");
         exit(0);
     }
-    t_String *new_str = MAKE_NEW(String);
-    new_str = string_init(new_str);
-    new_str->String.value = inpstr;
+    astring = string_value(insptr);
+    free(astring);
+    return astring;
 }
 
 void printer_print_float(t_Printer *this, t_Float *v_arg)
 {
     fprintf(this->Printer.target, "%ld\n", v_arg->Float.value);
 }
+
 void printer_print_integer(t_Printer *this, t_Integer *v_arg)
 {
     fprintf(this->Printer.target, "%d\n", v_arg->Integer.value);
 }
+
 void printer_print_string(t_Printer *this, t_String *v_arg)
 {
     fprintf(this->Printer.target, "%s\n", varg->String.value);
 }
-t_System global_system;
-int obj_counter;
+
+
+/* void system_exit(t_System *this, t_Integer *v_code) */
+/* t_System *system_init(t_System *this) */
+
