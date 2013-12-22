952efe5e6dd13b5458322a0cc76f0bce5c3de201
Matthew Maycock <ummaycoc@gmail.com>
2013-12-21 21:33:00 -0500

    Initialization for built ins.

--- ray/headers/gamma-builtin-meta.h
+++ ray/headers/gamma-builtin-meta.h
@@ -4,70 +4,29 @@ typedef struct {
         char** ancestors;
 } ClassInfo;
 
+void class_info_init(do var arg magic here) {
 
 
-ClassInfo M_BOOLEAN = {
-	.ancestors = {
-		m_classes[T_OBJECT ], m_classes[T_BOOLEAN ]
-	},
-	.generation = 1,
-	.class = m_classes[T_BOOLEAN ]
-};
+}
 
-ClassInfo M_FLOAT = {
-	.ancestors = {
-		m_classes[T_OBJECT ], m_classes[T_FLOAT ]
-	},
-	.generation = 1,
-	.class = m_classes[T_FLOAT ]
-};
+ClassInfo M_BOOLEAN;
+ClassInfo M_FLOAT;
+ClassInfo M_INTEGER;
+ClassInfo M_OBJECT;
+ClassInfo M_PRINTER;
+ClassInfo M_SCANNER;
+ClassInfo M_STRING;
+ClassInfo M_SYSTEM;
 
-ClassInfo M_INTEGER = {
-	.ancestors = {
-		m_classes[T_OBJECT ], m_classes[T_INTEGER ]
-	},
-	.generation = 1,
-	.class = m_classes[T_INTEGER ]
-};
-
-ClassInfo M_OBJECT = {
-	.ancestors = {
-		m_classes[T_OBJECT ]
-	},
-	.generation = 0,
-	.class = m_classes[T_OBJECT ]
-};
-
-ClassInfo M_PRINTER = {
-	.ancestors = {
-		m_classes[T_OBJECT ], m_classes[T_PRINTER ]
-	},
-	.generation = 1,
-	.class = m_classes[T_PRINTER ]
-};
-
-ClassInfo M_SCANNER = {
-	.ancestors = {
-		m_classes[T_OBJECT ], m_classes[T_SCANNER ]
-	},
-	.generation = 1,
-	.class = m_classes[T_SCANNER ]
-};
-
-ClassInfo M_STRING = {
-	.ancestors = {
-		m_classes[T_OBJECT ], m_classes[T_STRING ]
-	},
-	.generation = 1,
-	.class = m_classes[T_STRING ]
-};
-
-ClassInfo M_SYSTEM = {
-	.ancestors = {
-		m_classes[T_OBJECT ], m_classes[T_SYSTEM ]
-	},
-	.generation = 1,
-	.class = m_classes[T_SYSTEM ]
-};
-
+void init_built_in_infos() {
+	class_info_init(&M_BOOLEAN, 2, m_classes[T_OBJECT], m_classes[T_BOOLEAN]);
+	class_info_init(&M_FLOAT, 2, m_classes[T_OBJECT], m_classes[T_FLOAT]);
+	class_info_init(&M_FLOAT, 2, m_classes[T_OBJECT],m_classes[T_FLOAT]);
+	class_info_init(&M_INTEGER, 2, m_classes[T_OBJECT],m_classes[T_INTEGER]);
+	class_info_init(&M_OBJECT, 1, m_classes[T_OBJECT]);
+	class_info_init(&M_PRINTER, 2, m_classes[T_OBJECT],m_classes[T_PRINTER]);
+	class_info_init(&M_SCANNER, 2, m_classes[T_OBJECT],m_classes[T_SCANNER]);
+	class_info_init(&M_STRING, 2, m_classes[T_OBJECT],m_classes[T_STRING]);
+	class_info_init(&M_SYSTEM, 2, m_classes[T_OBJECT],m_classes[T_SYSTEM]);
+}
 
