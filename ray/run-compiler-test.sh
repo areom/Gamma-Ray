4850c5b45af85b8e730e83911e62476db9c42514
Matthew Maycock <ummaycoc@gmail.com>
2013-12-22 19:47:40 -0500

    Runs on all arguments now.

--- ray/run-compiler-test.sh
+++ ray/run-compiler-test.sh
@@ -5,15 +5,23 @@ function errwith {
   exit 1
 }
 
-test "$#" -lt 1 && errwith "Please give a file to test"
-file=$1
+function run_file {
+  test "$#" -lt 1 && errwith "Please give a file to test"
+  file=$1
 
-test -e "$file" || errwith "File $file does not exist."
-test -f "$file" || errwith "File $file is not a file."
+  test -e "$file" || errwith "File $file does not exist."
+  test -f "$file" || errwith "File $file is not a file."
 
-echo "=========================="
-echo "$file"
-cat "$file"
-echo "=========================="
-./bin/ray "$file" > ctest/test.c && cd ctest && ./compile && ./a.out Test
+  echo "================================================================"
+  echo "================================================================"
+  echo "$file"
+  cat "$file"
+  echo "================================================================"
+  echo "================================================================"
+  ./bin/ray "$file" > ctest/test.c && ( cd ctest && ./compile && ./a.out Test )
+}
+
+for afile in "${@}" ; do
+  run_file "$afile"
+done
 
